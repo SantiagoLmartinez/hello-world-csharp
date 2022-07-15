a1a54e3b96d4307fba4e553831a84c879a99e1ff
santiago L Martinez <slm17slm17@gmail.com>
2022-07-14 23:22:33 -0300

    comentamos el codigo

--- hello-world-csharp/Program.cs
+++ hello-world-csharp/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Mostramos mensaje por consola 
 Console.WriteLine("Hello, World!");
 
+//declaramos tipo de variable, nombre y le asignamos valor
 string saludo = "hello" ;
 string mundo = "world" ;
 
 Console.WriteLine("--------------------");
 
-
+//Mostramos valor de variable y  concatenamos
 Console.WriteLine(saludo + " " + mundo);
